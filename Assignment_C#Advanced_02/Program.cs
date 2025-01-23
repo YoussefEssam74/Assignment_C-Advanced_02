762cf14241c47e38a944b5088ca2a778559488c1
youssef_74 <youssefessam1293@gmail.com>
2025-01-23 15:22:19 +0200

    Q1 How many numbers in array that is greater than  X ?

--- Assignment_C#Advanced_02/Program.cs
+++ Assignment_C#Advanced_02/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-
+            #region Q1 How many numbers in array that is greater than  X ?
+           //int N = int.Parse(Console.ReadLine());
+           //
+           //int Q = int.Parse(Console.ReadLine());
+           //
+           //
+           //int[] array = new int[N];
+           //for (int i = 0; i < N; i++)
+           //{
+           //    array[i] = int.Parse(Console.ReadLine());
+           //}
+           //
+           //for (int i = 0; i < Q; i++)
+           //{
+           //    int X = int.Parse(Console.ReadLine());
+           //
+           //    int count = 0;
+           //    for (int j = 0; j < N; j++)
+           //    {
+           //        if (array[j] > X)
+           //        {
+           //            count++;
+           //        }
+           //    }
+           //
+           //    Console.WriteLine(count);
+           //} 
+            #endregion
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
