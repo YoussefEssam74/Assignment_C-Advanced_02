b2200407e1e672873ca287d09201f36b0ae4ee5b
youssef_74 <youssefessam1293@gmail.com>
2025-01-23 18:23:31 +0200

    ArrayList

--- Assignment_C#Advanced_02/Program.cs
+++ Assignment_C#Advanced_02/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Assignment_C_Advanced_02
+﻿using System.Collections;
+
+namespace Assignment_C_Advanced_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            #region Arraylist
+            // Console.WriteLine("--- ArrayList ---");
+            // ArrayList arrayList = new ArrayList();
+            // arrayList.Add(10); // Add elements
+            // arrayList.Add("Hello");
+            // arrayList.Add(3.14);
+            // Console.WriteLine("ArrayList Elements:");
+            // foreach (var item in arrayList)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            // Console.WriteLine("Contains 'Hello': " + arrayList.Contains("Hello"));
+            // arrayList.Remove(10); // Remove an element
+            // Console.WriteLine("After removing 10:");
+            // foreach (var item in arrayList)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            // Console.WriteLine();
+            #endregion
+           
         }
     }
 }
