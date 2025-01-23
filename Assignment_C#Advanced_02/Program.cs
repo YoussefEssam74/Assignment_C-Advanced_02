17ad1b844b8c6b9f4ff843e1f9f83e2a75043099
youssef_74 <youssefessam1293@gmail.com>
2025-01-23 18:32:52 +0200

    List

--- Assignment_C#Advanced_02/Program.cs
+++ Assignment_C#Advanced_02/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace Assignment_C_Advanced_02
 {
@@ -6,27 +9,57 @@ namespace Assignment_C_Advanced_02
     {
         static void Main(string[] args)
         {
-            #region Arraylist
-            // Console.WriteLine("--- ArrayList ---");
-            // ArrayList arrayList = new ArrayList();
-            // arrayList.Add(10); // Add elements
-            // arrayList.Add("Hello");
-            // arrayList.Add(3.14);
-            // Console.WriteLine("ArrayList Elements:");
-            // foreach (var item in arrayList)
-            // {
-            //     Console.WriteLine(item);
-            // }
-            // Console.WriteLine("Contains 'Hello': " + arrayList.Contains("Hello"));
-            // arrayList.Remove(10); // Remove an element
-            // Console.WriteLine("After removing 10:");
-            // foreach (var item in arrayList)
-            // {
-            //     Console.WriteLine(item);
-            // }
-            // Console.WriteLine();
-            #endregion
-           
+        #region Arraylist
+        // Console.WriteLine("--- ArrayList ---");
+        // ArrayList arrayList = new ArrayList();
+        // arrayList.Add(10); // Add elements
+        // arrayList.Add("Hello");
+        // arrayList.Add(3.14);
+        // Console.WriteLine("ArrayList Elements:");
+        // foreach (var item in arrayList)
+        // {
+        //     Console.WriteLine(item);
+        // }
+        // Console.WriteLine("Contains 'Hello': " + arrayList.Contains("Hello"));
+        // arrayList.Remove(10); // Remove an element
+        // Console.WriteLine("After removing 10:");
+        // foreach (var item in arrayList)
+        // {
+        //     Console.WriteLine(item);
+        // }
+        // Console.WriteLine();
+        //Methods:
+        //   Add(object): Adds an element to the ArrayList.
+        //
+        //   Remove(object): Removes the first occurrence of an element.
+        //
+        //    Contains(object): Checks if an element exists in the ArrayList.
+        #endregion
+        #region List
+        // Console.WriteLine("--- List<T> ---");
+        // List<int> list = new List<int> { 1, 2, 3, 4, 5 }; // Initialize with elements
+        // list.Add(6); // Add an element
+        // list.Remove(3); // Remove an element
+        // Console.WriteLine("List Elements:");
+        // foreach (int item in list)
+        // {
+        //     Console.WriteLine(item);
+        // }
+        // Console.WriteLine("Index of 4: " + list.IndexOf(4)); // Find the index of an element
+        // Console.WriteLine();
+
+        // Methods:
+
+        // Add(T): Adds an element to the list.
+        //
+        //Remove(T): Removes the first occurrence of an element.
+        //
+        // IndexOf(T): Returns the index of an element.
+        #endregion
+       
+
+
+
         }
     }
 }
